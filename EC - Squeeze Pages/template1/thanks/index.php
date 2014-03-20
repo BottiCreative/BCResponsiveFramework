a802014f15645c01baa27d96c01076c027762176
matteld80 <lankyman12>
2014-03-20 09:26:35 +0000

    Template Set Up

---
+++ EC - Squeeze Pages/template1/thanks/index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourFREE Squueze Page Template -1</title>
+    <meta name="description" content="Free Web Page Template">
+    <meta name="author" content="Botti Creative">
+    
+<!-- IE 8 or below -->   
+<!--[if lt IE 9]>
+<link rel="stylesheet" href="css/IEfoundation.css" />
+<![endif]-->
+
+    <link rel="stylesheet" href="../../css/squeeze-pages.css" />
+    <link rel="stylesheet" href="../../css/formStyles.css" />
+    <link rel="stylesheet" href="../../css/foundation.css" />
+    <script src="js/modernizr.js"></script>
+    
+<script type="text/javascript" src="//use.typekit.net/mjc5eoq.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+</head>
+  
+<body class="SqueezeThanks">
+
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N7D7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N7D7');</script>
+<!-- End Google Tag Manager -->
+
+
+
+
+
+<div class="rowNarrow Boxed">
+
+	<div class="grid-9 columns nopadRight">
+    
+    <h1>Hi <?php echo $_GET["Contact0FirstName"]; echo $_GET['FirstName']; echo $_GET["inf_field_FirstName"]; ?></h1>
+    <h2>Here's your FREE mobile responsive squeeze page</h2>
+    <p>Simply click the big button below to get the template</p>
+    
+     <a href="#" class="a_demo_four">GET YOUR FREE TEMPLATE NOW</a>
+          
+    
+    
+    
+    </div>
+    
+    <div class="grid-3 columns nopadLeft">
+    
+    <img src="../../images/holder-image.gif" alt="Template 1"/>
+    
+    
+    </div>
+    
+    
+    
+   <div class="row BottiFooter nopad"> 
+   
+     <div class="grid-8 columns nopad BottiInfo">
+     
+     <div class="grid-9 columns nopad">
+     <p class="smallerText">Your Free template was designed and built with love by the Entrepreneur's Circle's Super Sexy design arm...</p>
+     </div>
+     
+     <div class="grid-3 columns">
+         <a href="http://www.botticreative.co.uk" target="_blank"><img src="../../images/BottiCreativeLogo.gif" alt="Botti Creative"/></a>
+    
+     </div>
+     
+     
+     <div class="testWrapper">
+     <div class="testimonial">
+     
+     <p>Hi I'm Helen... the account manager at Botti Creative. If you need any help setting up or hosting your squeeze pages please them give me or the team a call to get started on <strong>0121 765 3406</strong> and we'll be more then happy to help.</p>
+     
+     </div>
+     
+     </div>
+     
+</div>
+   
+
+<div class="grid-4 columns"><img src="../../images/helen.png" alt="Helen Botti Creative Account Manager"/></div>
+
+</div>
+
+
+<p class="centered footCopy">&copy; Copyright Nigel Botterill's Entrepreneur's Circle and Botti Creative</p>
+
+
+
+</div>
+
+
+
+</body>
+</html>
